8333f2aadc23e9c9e8e108cc045f5ca64f2a9216
juliesundar <juliesundar123@gmail.com>
2022-11-12 09:58:13 -0300

    added

---
+++ websrver-espnow-sender/websrver-espnow-sender.ino
@@ -0,0 +1,236 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-esp-now-wi-fi-web-server/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+#include <esp_now.h>
+#include <esp_wifi.h>
+#include <WiFi.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <Wire.h>
+#include <BH1750.h>
+
+// Set your Board ID (ESP32 Sender #1 = BOARD_ID 1, ESP32 Sender #2 = BOARD_ID 2, etc)
+#define BOARD_ID 1
+#define POWER_CTRL          4
+#define BAT_ADC             33
+#define SALT_PIN            34
+#define SOIL_PIN            32
+#define I2C_SDA             25
+#define I2C_SCL             26
+// Digital pin connected to the DHT sensor
+#define DHTPIN 16  
+
+
+BH1750 lightMeter(0x23); //0x23
+
+// Uncomment the type of sensor in use:
+#define DHTTYPE    DHT11     // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
+
+//MAC Address of the receiver 
+uint8_t broadcastAddress[] = {0x24, 0x6F, 0x28, 0x9E, 0x8D, 0x5C};
+
+//Structure example to send data
+//Must match the receiver structure
+typedef struct struct_message {
+    int id;
+    float temp;
+    float hum;
+    float soil;
+    float salt;
+    float light;
+    float batt;
+    int readingId;
+} struct_message;
+
+//Create a struct_message called myData
+struct_message myData;
+
+unsigned long previousMillis = 0;   // Stores last time temperature was published
+const long interval = 10000;        // Interval at which to publish sensor readings
+
+unsigned int readingId = 0;
+
+// Insert your SSID
+constexpr char WIFI_SSID[] = "Virus";
+
+int32_t getWiFiChannel(const char *ssid) {
+  if (int32_t n = WiFi.scanNetworks()) {
+      for (uint8_t i=0; i<n; i++) {
+          if (!strcmp(ssid, WiFi.SSID(i).c_str())) {
+              return WiFi.channel(i);
+          }
+      }
+  }
+  return 0;
+}
+
+float readDHTTemperature() {
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  //float t = dht.readTemperature(true);
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(t)) {    
+    Serial.println("Failed to read from DHT sensor!");
+    return 0;
+  }
+  else {
+    Serial.println(t);
+    return t;
+  }
+}
+
+float readDHTHumidity() {
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  if (isnan(h)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return 0;
+  }
+  else {
+    Serial.println(h);
+    return h;
+  }
+}
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+ 
+void setup() {
+  //Init Serial Monitor
+  Serial.begin(115200);
+
+  dht.begin();
+  pinMode(POWER_CTRL, OUTPUT);
+  digitalWrite(POWER_CTRL, 1);  
+
+  //lux 
+  if (lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) {
+        Serial.println(F("BH1750 Advanced begin"));
+    } else {
+        Serial.println(F("Error initialising BH1750"));
+    }
+
+  // Set device as a Wi-Fi Station and set channel
+  WiFi.mode(WIFI_STA);
+
+  int32_t channel = getWiFiChannel(WIFI_SSID);
+
+  WiFi.printDiag(Serial); // Uncomment to verify channel number before
+  esp_wifi_set_promiscuous(true);
+  esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
+  esp_wifi_set_promiscuous(false);
+  WiFi.printDiag(Serial); // Uncomment to verify channel change after
+
+  //Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  //Register peer
+  esp_now_peer_info_t peerInfo;
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.encrypt = false;
+  
+  //Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+}
+// functions here 
+
+uint16_t readSoil()
+{
+    uint16_t soil = analogRead(SOIL_PIN);
+    return map(soil, 0, 4095, 100, 0);
+}
+
+uint32_t readSalt()
+{
+    uint8_t samples = 120;
+    uint32_t humi = 0;
+    uint16_t array[120];
+
+    for (int i = 0; i < samples; i++) {
+        array[i] = analogRead(SALT_PIN);
+        delay(2);
+    }
+    std::sort(array, array + samples);
+    for (int i = 0; i < samples; i++) {
+        if (i == 0 || i == samples - 1)continue;
+        humi += array[i];
+    }
+    humi /= samples - 2;
+    return humi;
+}
+
+float readBattery()
+{
+    int vref = 1100;
+    uint16_t volt = analogRead(BAT_ADC);
+    float battery_voltage = ((float)volt / 4095.0) * 2.0 * 3.3 * (vref);
+    return battery_voltage;
+}
+void loop() {
+    digitalWrite(POWER_CTRL, 1); // Turn power on dht
+    delay(1000);
+    
+    Serial.println("soil");
+    uint16_t soil = readSoil();
+    Serial.println(soil);
+    
+    Serial.println("salt");
+    uint32_t salt = readSalt();
+    Serial.println(salt);
+    Serial.println("light");
+   float light = lightMeter.readLightLevel();
+    Serial.println(light);
+    Serial.println("batt");
+    float batt = readBattery();
+    Serial.println(batt);
+    
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    // Save the last time a new reading was published
+    previousMillis = currentMillis;
+    //Set values to send
+    myData.id = BOARD_ID;
+    myData.temp = readDHTTemperature();
+    myData.hum = readDHTHumidity();
+    myData.soil = readSoil();
+    myData.salt = readSalt();
+    myData.light = lightMeter.readLightLevel();
+    myData.batt = readBattery();
+    myData.readingId = readingId++;
+     
+    //Send message via ESP-NOW
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+    if (result == ESP_OK) {
+      Serial.println("Sent with success");
+    }
+    else {
+      Serial.println("Error sending the data");
+      
+    }
+  }
+    digitalWrite(POWER_CTRL, 1); // Turn power on dht
+}
